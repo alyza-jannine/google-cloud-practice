b577d99bedc32932dda0e0a1de8db4a4e31270e4
alyza-jannine <64810215+alyza-jannine@users.noreply.github.com>
2020-05-06 09:03:36 +0800

    Add files via upload
    Using Google Cloud Functions

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import request, jsonify
+
+def add_record(request):    
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
+    name = request.form['Name']
+    gbu = request.form['GBU']
+    resp = {'Name': name, 'GBU': gbu}
+
+    return (jsonify(resp), 200, headers)
